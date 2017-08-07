9112ea1ce80990f501fd959032891a7b76df4aba
Юрий Бондарчук <YuryBandarchuk@Urijs-MacBook-Pro.local>
2017-08-07 14:18:55 +0100

    Initial Commit

---
+++ RxFlickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxFlickr
+//
+//  Created by Юрий Бондарчук on 07/08/2017.
+//  Copyright © 2017 Yury Bandarchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
